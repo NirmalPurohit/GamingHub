08611f23fe85ad4ffc93aaf7ab6075297e796442
Nirmal Purohit <47617070+NirmalStark@users.noreply.github.com>
2019-03-12 21:03:05 -0400

    Removed comments

--- app/src/main/java/com/example/nirmal/gaminghub/DetailsOfGame.java
+++ app/src/main/java/com/example/nirmal/gaminghub/DetailsOfGame.java
@@ -30,7 +30,6 @@ public class DetailsOfGame extends AppCompatActivity {
 
     public String gameID="";
     TextView tv=null;
-   // ImageView img=null;
     FrameLayout ll=null;
     Button download=null;
 
@@ -41,13 +40,10 @@ public class DetailsOfGame extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         tv = (TextView) findViewById(R.id.detail);
-      //  img=(ImageView) findViewById(R.id.detailimage);
         ll=(FrameLayout) findViewById(R.id.rl);
         Bundle bundle = getIntent().getExtras();
         final String value = bundle.getString("gameid");
-        //tv.setText(value);
         gameID = value;
-       // Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG).show();
         new ViewDetailsTask(DetailsOfGame.this).execute();
         download=(Button) findViewById(R.id.dwnld);
         download.setOnClickListener(new View.OnClickListener()
@@ -62,8 +58,6 @@ public class DetailsOfGame extends AppCompatActivity {
                                        }
                                     }
         );
-       // Toast.makeText(getApplicationContext(), "hello there", Toast.LENGTH_LONG).show();
-       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public class ViewDetailsTask extends AsyncTask<Void, Void, Void> {
@@ -80,7 +74,6 @@ public class DetailsOfGame extends AppCompatActivity {
             try {
                 String gmID=gameID;
                 URL openUrl = new URL("http://192.168.1.4:8080/GamingHub/ShowDataServlet");
-               // Toast.makeText(getApplicationContext(), "in async task ", Toast.LENGTH_LONG).show();
 
                 HttpURLConnection connection = (HttpURLConnection) openUrl.openConnection();
 
@@ -88,25 +81,21 @@ public class DetailsOfGame extends AppCompatActivity {
                 connection.setDoInput(true);
                 connection.setDoOutput(true);
                 OutputStream os=connection.getOutputStream();
-               // Log.d("After URL","here");
                 BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
                 String data=URLEncoder.encode("gameid","UTF-8")+"="+URLEncoder.encode(gmID,"UTF-8");
                 bw.write(data);
                 bw.close();
-             //   connection.setDoInput(true);
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(),"iso-8859-1"));
 
                 String line = "";
 
                 while ((line = br.readLine()) != null) {
                     getOutput.append(line+"\n");
-                   // Log.d("AsyncTask", line);
                 }
                 br.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
-                // Toast.makeText(getApplicationContext(), "in async task exception", Toast.LENGTH_LONG).show();
             }
             return null;
         }
@@ -115,7 +104,6 @@ public class DetailsOfGame extends AppCompatActivity {
 
             tv.setText(getOutput);
             ll.setBackgroundResource(R.drawable.image);
-           // img.setBackgroundResource(R.drawable.image);
         }
     }
 
