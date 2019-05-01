ce6eaf5b0068e39a027973c9ca64c9eb37140472
Nirmal Purohit <47617070+NirmalPurohit@users.noreply.github.com>
2019-04-30 22:20:49 -0400

    Aligned the code

--- app/src/main/java/com/example/nirmal/gaminghub/MyPurchaseFragment.java
+++ app/src/main/java/com/example/nirmal/gaminghub/MyPurchaseFragment.java
@@ -1,6 +1,5 @@
 package com.example.nirmal.gaminghub;
 
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -27,8 +25,6 @@ import java.net.URLEncoder;
  * A simple {@link Fragment} subclass.
  */
 public class MyPurchaseFragment extends Fragment {
-
-
     EditText username=null;
     EditText password=null;
     Button login=null;
@@ -36,15 +32,11 @@ public class MyPurchaseFragment extends Fragment {
     String pass=null;
     TextView content=null;
 
-    public MyPurchaseFragment() {
-        // Required empty public constructor
-    }
-
+    public MyPurchaseFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_purchase, container, false);
     }
 
@@ -59,60 +51,40 @@ public class MyPurchaseFragment extends Fragment {
                                          uname = username.getText().toString();
                                          pass = password.getText().toString();
                                          new LoginPurchaseTask().execute();
-
-
                                      }
                                  }
         );
-
     }
 
     public class LoginPurchaseTask extends AsyncTask<Void, Void, Void> {
         int flag = 0;
         StringBuilder output=new StringBuilder();
         protected Void doInBackground(Void... params) {
-
             URL openUrl = null;
-
             try {
-                openUrl = new URL("http://192.168.1.4:8080/GamingHub/ShowPurchase");
-
-                // Toast.makeText(getApplicationContext(), "in async task ", Toast.LENGTH_LONG).show();
-
+                openUrl = new URL("http://<IP Adress>:8080/GamingHub/ShowPurchase");
                 HttpURLConnection connection = (HttpURLConnection) openUrl.openConnection();
-
                 connection.setRequestMethod("POST");
-                // connection.setDoInput(true);
                 connection.setDoOutput(true);
                 OutputStream os = connection.getOutputStream();
-               // Log.d("After URL", "here");
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "iso-8859-1"));
-               // Log.d("Button", "calling Async");
                 String data = URLEncoder.encode("uname", "UTF-8") + "=" + URLEncoder.encode(uname, "UTF-8") + "&" + URLEncoder.encode("pass", "UTF-8") + "=" + URLEncoder.encode(pass, "UTF-8");
-
                 bw.write(data);
                 bw.close();
-                //   connection.setDoInput(true);
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "iso-8859-1"));
-
                 String line = "";
-
                 while ((line = br.readLine()) != null) {
                    output.append(line+"\n");
                 }
-                // Log.d("AsyncTask", line);
                 br.close();
-
-            } catch (Exception e) {
+            } 
+            catch(Exception e) {
             }
-
             return null;
         }
-
+        
         public void onPostExecute(Void result) {
-
                 content.setText(output);
         }
     }
-
 }
