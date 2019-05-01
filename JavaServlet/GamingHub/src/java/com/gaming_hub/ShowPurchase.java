f57a9115799d551e9a0eb7a05713b78cb80d02f1
Nirmal Purohit <47617070+NirmalPurohit@users.noreply.github.com>
2019-04-30 21:28:54 -0400

    Aligned the code

--- JavaServlet/GamingHub/src/java/com/gaming_hub/ShowPurchase.java
+++ JavaServlet/GamingHub/src/java/com/gaming_hub/ShowPurchase.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.gaming_hub;
 
 import java.io.File;
@@ -48,15 +43,10 @@ public class ShowPurchase extends HttpServlet {
             Connection con=DriverManager.getConnection(  
 				"jdbc:mysql://localhost:3306/gaming_hub_db","root","");
             String sql="SELECT * from customer where cust_name=? and cust_key=?";
-           // String ID=request.getParameter("gameid");
             PreparedStatement ps=con.prepareStatement(sql);
             ps.setString(1,name);
             ps.setString(2, pass);
             ResultSet rs=ps.executeQuery();
-            //response.setContentType("image/jpg");
-           // DataOutputStream dout=new DataOutputStream();
-           
-            
             while(rs.next())
             {
                 out.println(rs.getString(2)+"\n");
@@ -65,8 +55,6 @@ public class ShowPurchase extends HttpServlet {
             }
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
